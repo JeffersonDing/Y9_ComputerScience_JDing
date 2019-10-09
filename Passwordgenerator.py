56fe63317686f0a10b9ea46558ab463020cdc833
Jefferson Ding <jefferson.ding@ucc.on.ca>
2019-10-08 21:08:57 -0400

    Password Generator Test Attempt #1

--- Passwordgenerator.py
+++ Passwordgenerator.py
@@ -1,17 +1,24 @@
 import os
+
+
 print("\nWelcome to Random Password Generator!")
 print("please select the option / options you want!")
 print("Eg. : For Letters and Numbers Enter '1' press enter then input '2' \n\n")
 print("after you finish selecting enter'5'\n ")
-
+def showSel(Select):
+	print("\n\tyou selection(s) is(are) "
+	for y in range(len(select))
+		print(select)
 option = ["1.Letters","2.Numbers","3.Signs","4.Caps","5.Done"]
+selection = []
 for x in option:
 		print(x)
 
-select = []
-while( choise != 5):
-	choise = int(input("Please enter:"))
-	select.append(choise) 
-	print(select)
+for x in range(4):
+	a= int(input("please enter:"))
+	selection.append(a)
+	if(a==5):
+		break
 
-		
+	
+
