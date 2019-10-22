ee1fe920df99bb1a2e35748cafd3186289a13b28
Jefferson Ding <jefferson.ding@ucc.on.ca>
2019-10-22 12:13:16 -0400

    Main Password Generator update

--- Passwordgenerator.py
+++ Passwordgenerator.py
@@ -1,22 +1,23 @@
 import os
 
+def printsel(select):
+	os.system("clear")
+	print("Selected:")
+	for x in selection:
+		print(option[x-1])
 
 print("\nWelcome to Random Password Generator!")
 print("please select the option / options you want!")
 print("Eg. : For Letters and Numbers Enter '1' press enter then input '2' \n\n")
 print("after you finish selecting enter'5'\n ")
-def showSel(Select):
-	print("\n\tyou selection(s) is(are) "
-	for y in range(len(select))
-		print(select)
 option = ["1.Letters","2.Numbers","3.Signs","4.Caps","5.Done"]
 selection = []
 for x in option:
-		print(x)
-
+	print(x)
 for x in range(4):
 	a= int(input("please enter:"))
 	selection.append(a)
+	printsel(selection)
 	if(a==5):
 		break
 
