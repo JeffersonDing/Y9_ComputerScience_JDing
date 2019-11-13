83723b70b7be0874132d967cb125414ca3ab192b
JeffersonDing <tyding2004@outlook.com>
2019-11-13 13:58:40 -0500

    Update GUI interface with Frames

--- Python-Random-Password-Generator-master/runGUI.py
+++ Python-Random-Password-Generator-master/runGUI.py
@@ -1,5 +1,14 @@
 from tkinter import *
 root = Tk()
+
+
+#Frame Creation
+generatorf = Frame(root)
+vaultf = Frame(root)
+managerf = Frame(root)
+
+
+#Variable Declariation 
 Letter = BooleanVar()
 Numbers= BooleanVar()
 Caps= BooleanVar()
@@ -7,26 +16,31 @@ Special_Characters= BooleanVar()
 Easy_to_Read= BooleanVar()
 Easy_to_Memorize= BooleanVar()
 
-cbLetters = Checkbutton(text="Letters", variable=Letter,onvalue = True, offvalue = False, height = 2,width = 15)
-cbNumbers = Checkbutton(text="Numbers", variable=Numbers,onvalue = True, offvalue = False, height = 2,width = 15)
-cbCaps = Checkbutton(text="Caps", variable=Caps,onvalue = True, offvalue = False, height = 2,width = 15)
-cbSpecial = Checkbutton(text="Special Characters", variable=Special_Characters,onvalue = True, offvalue = False, height = 2,width = 15)
-cbEasyR = Checkbutton(text="Easy to Read", variable=Easy_to_Read,onvalue = True, offvalue = False, height = 2,width = 15)
-cbEasyM = Checkbutton(text="Easy to Memorize", variable=Easy_to_Memorize,onvalue = True, offvalue = False, height = 2,width = 15)
-lbTitle = Label(text = "Random Password Generator")
-lbPassword = Entry(text = "Pleas Enter Password")
-btnGo = Button(text = "Go !")
+
+#Generator Frame Create and Configure
+cb = []
+cb.append(Checkbutton(generatorf,text="Letters", variable=Letter,onvalue = True, offvalue = False, height = 2,width = 15))
+cb.append(Checkbutton(generatorf,text="Numbers", variable=Numbers,onvalue = True, offvalue = False, height = 2,width = 15))
+cb.append(Checkbutton(generatorf,text="Caps", variable=Caps,onvalue = True, offvalue = False, height = 2,width = 15))
+cb.append(Checkbutton(generatorf,text="Special Characters", variable=Special_Characters,onvalue = True, offvalue = False, height = 2,width = 15))
+cb.append(Checkbutton(generatorf,text="Easy to Read", variable=Easy_to_Read,onvalue = True, offvalue = False, height = 2,width = 15))
+cb.append(Checkbutton(generatorf,text="Easy to Memorize", variable=Easy_to_Memorize,onvalue = True, offvalue = False, height = 2,width = 15))
+lbTitle = Label(generatorf,text = "Random Password Generator")
+lbPassword = Entry(generatorf,text = "Pleas Enter Password")
+btnGo = Button(generatorf,text = "Go !")
 lbTitle.grid(row = 0,column = 0)
 lbPassword.grid(row = 1, column = 0)
 btnGo.grid(row = 1, column = 1)
-cbSpecial.grid(row = 2,column = 0)
-cbCaps.grid(row = 3, column = 0)
-cbNumbers.grid(row = 4, column = 0)
-cbLetters.grid(row = 5, column = 0)
-cbEasyR.grid(row = 6, column = 0)
-cbEasyM.grid(row = 7, column = 0)
-cbSpecial.grid(row = 8, column = 0)
+for x in range(0,6):
+	cb[x].grid(row = x+2, column = 0)
 
 
+#Vault Frame Create and Configure
 
+
+#Password Manager Create and Configure
+
+
+#Frame Pack
+generatorf.pack()
 root.mainloop()
