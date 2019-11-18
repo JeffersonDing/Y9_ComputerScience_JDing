4666ee00f0e52af504bb14e8caf7d33308cd0db2
JeffersonDing <tyding2004@outlook.com>
2019-11-18 09:58:38 -0500

    Update GUI and RSA interface

--- Python-Random-Password-Generator-master/GUI interface/runGUI.py
+++ Python-Random-Password-Generator-master/GUI interface/runGUI.py
@@ -3,7 +3,13 @@ import random
 from functools import partial
 import random
 root = Tk()
+#RSA Encryption
+cipher = []
+n=7441826991206406709576923798652306546773056383923657355748125433921170166237822700978602733318243198872391448038717383645273135292865009135623454648457251152479774667822283716405812196213269778591708092613681639094858652600822484100505704226715130775144353675554189536174751655752733823725881192996003709612306625816000913962672752947794794592929812789552418862834752848089722689574309164356302742239632448304296692333917423975797637051507227100217475335583330661060121209180098107074150139550375959262626119833501770072868699207069770140058936323994415274097579352438105654231135473547937006233631692474761658579637
+e=6553
+d=4165515245497497300584183807943943295218002566188306909947218692449695127385981026581644258478760270633897731024876447918642127308748489777119919372888935941903832364042749377655504217260838325633203919381502251213175879404824793480948408836195803400462305704552535818508925541477342845326801803129763712323707922729320122183190848453201638965312192858220393090816213720436537019479473566613916791146451770874471143677665974186271603566727889259702727174612886772425044819676162383477897768349836907574509704980372425128806961289095835294370498521487628563707891665120639474055335033475762900708067357970775016255657
 #Variable Declariation 
+vault = open("User_Data.txt","a+")
 Letter = BooleanVar()
 Numbers= BooleanVar()
 Caps= BooleanVar()
@@ -20,10 +26,10 @@ SMemo = StringVar()
 word = []
 chars = []
 generate = []
-special = ['!','@','#','$','%','&_','-','?']
+special = ['!','@','#','$','%','&_','-','?','!','@','#','$','%','&_','-','?','!','@','#','$','%','&_','-','?']
 ascii_uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 ascii_lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 #Button Functions
 def removeList(b):
@@ -56,44 +62,39 @@ def appendS():
 	else:
 		removeList(special)
 def Easy():
-	global chars
 	global word
-	if(Easy_to_Read.get()==True):
-		word = []
-		for i in range(0,26):
-			word.append(random.choice(open('English_noun.txt').readlines()).strip('\n'))
-		print(word)
-		chars.extend(word)
-	else:
+	if(Easy_to_Read.get()==False):
 		removeList(word)
-def Refresh():
-	global chars
-	global word
-	if(Easy_to_Read.get()==True):
-		word = []
-		for i in range(0,26):
-			word.append(random.choice(open('English_noun.txt').readlines()).strip('\n'))
-		print(word)
-		chars.extend(word)
-	else:
-		return
 def Create():
 	global generate
+	global chars
+	global word
 	generate = []
+	removeList(word)
+	if(Easy_to_Read.get()==True):
+		word = []
+		for i in range(0,26):
+			word.append(random.choice(open('English_noun.txt').readlines()).strip('\n'))
+		chars.extend(word)
 	for i in range(0,Size.get()):
 		generate.append(random.choice(chars))
 	Generate.set(''.join(generate))
+	print(chars)
 	
 def Fill():
 	Password.set(Generate.get())
+
+def Encrypt_Save():
+	plaintext=Password.get()
+	for x in plaintext:
+		cipher.append(str(pow(ord(x),e,n)))
+	vault.write(str('\t'.join(cipher)))
+
 
 #Frame Creation
 generatorf = Frame(root)
 vaultf = Frame(root)
 managerf = Frame(root)
-
-
-
 
 #Generator Frame Create and Configure
 cb = []
@@ -107,13 +108,11 @@ lbGSize = Label(generatorf,text = "Size")
 lbGTitle = Label(generatorf,text = "Random Password Generator")
 EtGPassword = Entry(generatorf,text = "Pleas Enter Password",textvariable = Generate)
 btnGGo = Button(generatorf,text = "Go !",command = Create )
-btnGRefresh = Button(generatorf,text = "Refresh",command = Refresh )
 lbGSize.grid(row =2,column = 1)
 EtGSize.grid(row = 2,column = 0)
 lbGTitle.grid(row = 0,column = 0)
 EtGPassword.grid(row = 1, column = 0)
 btnGGo.grid(row = 1, column = 1)
-btnGRefresh.grid(row = 7,column = 1)
 for x in range(0,5):
 	cb[x].grid(row = x+3, column = 0)
 
@@ -125,7 +124,7 @@ LbVUsername =Label(vaultf,text = "Username")
 EtVUsername = Entry(vaultf,text = "Username ... ",textvariable = Username)
 LbVMemo = Label(vaultf,text = "Memo")
 EtVMemo = Entry(vaultf,text = "Memo ...",textvariable = Memo)
-BtnVSave = Button(vaultf,text = "Save !")
+BtnVSave = Button(vaultf,text = "Save !",command = Encrypt_Save)
 BtnVFill = Button(vaultf,text = "Fill from Generator",command = Fill)
 LbVPassword.grid(row = 1, column = 0)
 EtVPassword.grid(row = 1, column = 1)
