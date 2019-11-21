c79b6735993be534bb951e3a95faaca41617de9c
JeffersonDing <tyding2004@outlook.com>
2019-11-21 17:28:57 -0500

    multiple page test

--- Python-Random-Password-Generator/GUI interface/test.py
+++ Python-Random-Password-Generator/GUI interface/test.py
@@ -1,8 +1,19 @@
-import csv
-from pynput.keyboard import Key,Controller
-import time
-csvfile = open('User_Data.csv', 'r+')
-a = csvfile.readlines(1)
-print(a)
-if(a==['Username,Memo,Password\n']):
-	print('Obay')
+import tkinter as tk
+def forget():
+	try:
+		p2.pack_forget()
+	except:
+		print('gone')
+root = tk.Tk()
+p1 = tk.Frame()
+p2 = tk.Frame()
+t1 = tk.Label(p1,text = "Hello")
+t1.pack()
+t2 = tk.Label(p2,text = 'Hi')
+t2.pack()
+b1 = tk.Button(p1,text = "forget",command = forget)
+b1.pack()
+p1.pack(side = 'left')
+p2.pack(side = 'right')
+
+root.mainloop()
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Stores Year 9 Python Files
 Jefferson Ding | Jackson's
 
 =======
+
 Unit 1 Final Project
 
 /Year-9-Design-Python-JD/Python-Random-Password-Generator/GUI interface
