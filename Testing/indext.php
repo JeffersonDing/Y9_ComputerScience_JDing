5546cfe255f90b7ee654f7d7894af00e8d0826b7
JeffersonDing <tyding2004@outlook.com>
2020-02-24 21:56:35 -0500

    update UNSDG page

---
+++ Testing/indext.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Testing Page</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+</head>
+<body>
+<canvas id="myChart" width="400" height="200"></canvas>
+</body>
+<script type="text/javascript" src="script.js"></script>s
+
+</html>
