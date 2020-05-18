c3a30b816681e0003ae1a0c4c818df36591345d8
JeffersonDing <tyding2004@outlook.com>
2020-05-18 00:02:16 -0400

    server update

---
+++ src/City.java
@@ -0,0 +1,26 @@
+//City / Town Setup
+public class City {
+    private int centerX;
+    private int centerY;
+
+    public City(int centerX, int centerY) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+}
---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+//Simulation Setups
+public class Constants {
+
+    public static int ORIGINAL_COUNT = 50;//Initial Infected
+    public static float RATE = 0.8f;//Rate
+    public static float INCUBATION_TIME = 140;//Incubation Period
+    public static int HOSPITAL_RESPONSE_TIME = 10;//Hospital Response Time
+    public static int BED_COUNT = 4000;//Hospital Capacity
+    public static float SAFE_DIST = 2f;
+    public static int TOTAL_SUSPECTS = 5000;//Total Suspects
+    public static float FATALITY_RATE = 0.50f;//Fatality Rate
+    public static int DEATH_TIME = 100;//Death Time
+
+
+    public static final int CITY_WIDTH = 700;
+    public static final int CITY_HEIGHT = 800;
+
+}
