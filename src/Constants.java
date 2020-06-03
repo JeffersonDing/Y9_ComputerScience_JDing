b22a335d55986770676d62d81f53b850c098f59d
JeffersonDing <tyding2004@outlook.com>
2020-06-02 22:53:29 -0400

    upload update

--- src/Constants.java
+++ src/Constants.java
@@ -1,11 +1,11 @@
 //Simulation Setups
 public class Constants {
 
-    public static int ORIGINAL_COUNT = 50;//Initial Infected
-    public static float RATE = 0.8f;//Rate
+    public static int ORIGINAL_COUNT = 100;//Initial Infected
+    public static float RATE = 0.9f;//Rate
     public static float INCUBATION_TIME = 140;//Incubation Period
     public static int HOSPITAL_RESPONSE_TIME = 10;//Hospital Response Time
-    public static int BED_COUNT = 4000;//Hospital Capacity
+    public static int BED_COUNT = 1000;//Hospital Capacity
     public static float SAFE_DIST = 2f;
     public static int TOTAL_SUSPECTS = 5000;//Total Suspects
     public static float FATALITY_RATE = 0.50f;//Fatality Rate
