db06e821e93ca69c52709ab2cb246b2e0c35944e
JeffersonDing <tyding2004@outlook.com>
2020-11-14 22:31:44 -0500

    subtree add

--- git.sh
+++
@@ -1,3 +0,0 @@
-git subtree add --prefix Unit1 git@github.com:JeffersonUCC/Y9_ComputerScience_JDing.git master
-git subtree add --prefix Unit2 git@github.com:JeffersonUCC/jeffersonucc.github.io.git master
-git subtree add --prefix Unit3 git@github.com:JeffersonUCC/COVID-Simulation.git master
